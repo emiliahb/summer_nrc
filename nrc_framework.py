e35c60073eba5319365e4c5cb28ba47686c9e8c2
kareyzheng <karey.zheng@nyu.edu>
2018-07-18 14:00:31 -0400

    CHG

--- nrc_framework.py
+++ nrc_framework.py
@@ -1,4 +1,5 @@
 import requests
+import random
 
 withOptions = []
 
@@ -23,8 +24,12 @@ def tick():
     print(get_status())
     if get_status()['number_of_options'] > 1:
         withOptions.append([get_status()['player_position'], get_status()['options']])
-        index = input("Direction? ")
-        direct(index)
+
+        if 'Lung' in get_status()['options']:
+            direct(get_status()['options'].find("Lung"))
+        else:
+            index = random.randint(0, get_status()['number_of_options']-1)
+            direct(index)
 
 
 def direct(index):
@@ -42,9 +47,26 @@ def stop_hold():
     requests.post('http://10.42.0.1:5000/game/kz912/donuts/stop_holding')
     print(get_status())
 
+def seen():
+    turns_held = 0
+    while turns_held < 7 and get_status()['bot_visible'] == True:
+        if get_status()['options'] == get_status()['bot_location']:
+            tick()
+        if get_status()['player_position'] == get_status()['bot_location']:
+            break
+        hold()
+        tick()
+        turns_held += 1
+
+
+
 
 def main():
     reset()
+    #login()
+    #while get_status()['bot_visible'] != True:
+        #tick()
+    #seen()
     #print("bye")
 
 
