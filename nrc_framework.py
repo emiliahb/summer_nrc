2699957b05fa7ff13929d93486d3fc4b8434ce2c
kareyzheng <karey.zheng@nyu.edu>
2018-07-18 12:14:55 -0400

    CHG

--- nrc_framework.py
+++ nrc_framework.py
@@ -1,7 +1,54 @@
 import requests
 
-r = requests.post("http://10.42.0.1:5000/login/kz912/seecret")
-#this logs the user in, replace kz912 with a netID of your choice
-#"seecret" is the password for the netID's "account," can be any combination of numbers/letters
+withOptions = []
 
-print(r.text)
+
+def get_status():
+    info = requests.get('http://10.42.0.1:5000/game/kz912/donuts/get_status')
+    return info.json()
+
+
+def login():
+    requests.post('http://10.42.0.1:5000/login/kz912/donuts')
+    print(get_status())
+
+
+def reset():
+    requests.post('http://10.42.0.1:5000/game/kz912/donuts/reset')
+    print(get_status())
+
+
+def tick():
+    requests.post('http://10.42.0.1:5000/game/kz912/donuts/tick')
+    print(get_status())
+    if len(get_status()['number_of_options']) > 1:
+        withOptions.append([get_status()['player_position'], get_status()['options']])
+        index = input("Direction? ")
+        direct(index)
+
+
+def direct(index):
+    requests.post('http://10.42.0.1:5000/game/kz912/donuts/select_direction/' + str(index))
+    print(get_status())
+
+
+def hold():
+    requests.post('http://10.42.0.1:5000/game/kz912/donuts/start_holding')
+    print(get_status())
+
+
+def stop_hold():
+    requests.post('http://10.42.0.1:5000/game/kz912/donuts/stop_holding')
+    print(get_status())
+
+
+def main():
+    login()
+    #while get_status()['bot_visible'] != True and get_status()['rounds_remaining'] > 1000:
+        #tick()
+    print("bye")
+    reset()
+
+
+if __name__ == "__main__":
+    main()
