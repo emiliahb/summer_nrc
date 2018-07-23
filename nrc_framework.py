1af4d587e562bd3d4572539690e42a5d25b6ca4f
emiliahb <emilka.b3@gmail.com>
2018-07-23 15:11:26 -0400

    changed seen function (actually_seen) and began work on trapping self/bot in heart/lungs loop

--- nrc_framework.py
+++ nrc_framework.py
@@ -22,14 +22,23 @@ def reset():
 def tick():
     requests.post('http://10.42.0.1:5000/game/kz912/donuts/tick')
     print(get_status())
+    if(get_status()['player_position'] == "Heart_3"):
+        return
+
     if get_status()['number_of_options'] > 1:
         withOptions.append([get_status()['player_position'], get_status()['options']])
 
-        if 'Lung' in get_status()['options']:
-            direct(get_status()['options'].find("Lung"))
-        else:
-            index = random.randint(0, get_status()['number_of_options']-1)
-            direct(index)
+        for index in range(len(get_status()['options'])):
+            if (get_status()['options'][index].find('Heart') != -1):
+                direct(index)
+                return
+            elif(get_status()['options'][index].find('Lung') != -1):
+                direct(index)
+                return
+
+
+        index = random.randint(0, get_status()['number_of_options']-1)
+        direct(index)
 
 
 def direct(index):
@@ -50,24 +59,42 @@ def stop_hold():
 def seen():
     turns_held = 0
     while turns_held < 7 and get_status()['bot_visible'] == True:
-        if get_status()['options'] == get_status()['bot_location']:
+        if get_status()['options'] == get_status()['bot_location'] or get_status()['player_position'] != 'Heart_3':
+            stop_hold()
             tick()
-        if get_status()['player_position'] == get_status()['bot_location']:
-            break
+        elif get_status()['player_position'] == get_status()['bot_location']:
+            return
         hold()
         tick()
         turns_held += 1
+    stop_hold()
 
+def actually_seen():
+    if get_status()['player_position'] == "Heart_3":
+        #do this
+        hold()
+        while get_status()['player_position'] != get_status()['bot_location']:
+            tick()
+        stop_hold()
+        return
+    else:
+        #do something
+        while(get_status()['player_position'] != get_status()['bot_location']):
+            if get_status()['player_position'] == "Heart_3":
+                actually_seen()
+            else:
+                tick()
 
 
 
 def main():
     reset()
-    #login()
-    #while get_status()['bot_visible'] != True:
-        #tick()
-    #seen()
-    #print("bye")
+    login()
+    while get_status()['bot_visible'] != True and get_status()['player_position'] != "Heart_3":
+        tick()
+    actually_seen()
+    print(withOptions)
+    print("bye")
 
 
 
